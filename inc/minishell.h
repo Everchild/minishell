c30ed20669e0a696819f526ada444fd9afa1cda8
Everchild <sarah.justine.brochard@gmail.com>
2017-08-06 05:17:41 +0200

    modifs from home

--- inc/minishell.h
+++ inc/minishell.h
@@ -18,6 +18,24 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 
+typedef enum		e_built
+{
+	B_CD,
+	B_ECHO,
+	B_ENV,
+	B_SETENV,
+	B_UNSETENV,
+	B_EXIT,
+	B_COUNT
+}					t_built;
+
+typedef struct		s_builtins
+{
+	t_built			built;
+	char			*cmd;
+	int				(*builtin)(char **);
+}					t_builtins;
+
 void				get_env(t_dblist **save, char **orig, char *exe);
 void				get_underscore_var(t_dblist **save, char *exe);
 void				get_pwd_var(t_dblist **save);
--- src/main.c
+++ src/main.c
@@ -12,12 +12,84 @@
 
 #include <minishell.h>
 
-static void			execute_child(/*t_dblist **env, */char **cmd)
+static int			ft_cd(char **cmd)
 {
-	execve(*cmd, cmd, NULL);
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
 }
 
-static void			handle_cmd(/*t_dblist **env, */char **cmd)
+static int			ft_echo(char **cmd)
+{
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
+}
+
+static int			ft_env(char **cmd)
+{
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
+}
+
+static int			ft_setenv(char **cmd)
+{
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
+}
+
+static int			ft_unsetenv(char **cmd)
+{
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
+}
+
+static int			ft_exit_sh(char **cmd)
+{
+	ft_printf("builtin %s\n", *cmd);
+	return (0);
+}
+
+static t_builtins	*get_builtins(void)
+{
+	static t_builtins	ret[B_COUNT] = {
+		{ B_CD, "cd", &ft_cd },
+		{ B_ECHO, "echo", &ft_echo },
+		{ B_ENV, "env", &ft_env },
+		{ B_SETENV, "setenv", &ft_setenv },
+		{ B_UNSETENV, "unsetenv", &ft_unsetenv },
+		{ B_EXIT, "exit", &ft_exit_sh },
+	};
+
+	return (ret);
+}
+
+static void			exec_relative_path(t_dblist **env, char **cmd)
+{
+	ft_printf("%s\n", (*env)->start->content);
+	ft_printf("find path for %s\n", *cmd);
+}
+
+static void			execute_child(t_dblist **env, char **cmd)
+{
+	t_builtins		*builtins;
+	int				i;
+
+	builtins = get_builtins();
+	i = 0;
+	while (i < B_COUNT)
+	{
+		if (!ft_strcmp(*cmd, builtins[i].cmd))
+			break ;
+		i++;
+	}
+	if (i < B_COUNT)
+		builtins[i].builtin(cmd);
+	else if (ft_strchr(*cmd, '/'))
+		execve(*cmd, cmd, NULL);
+	else
+		exec_relative_path(env, cmd);
+}
+
+static void			handle_cmd(t_dblist **env, char **cmd)
 {
 	// test cmd
 	int				i = 0;
@@ -36,7 +108,7 @@ static void			handle_cmd(/*t_dblist **env, */char **cmd)
 		ft_printf("%s: unable to launch process\n", *cmd);
 	else if (pid == 0)
 	{
-		execute_child(cmd);
+		execute_child(env, cmd);
 		exit(0);
 	}
 	else
@@ -78,7 +150,7 @@ int					main(int argc, char **argv, char **env)
 			tmp = ft_strtrimall(buf);
 			cmd = ft_strsplit(tmp, ' ');
 			ft_strdel(&buf);
-			handle_cmd(/*&env_copy, */cmd);
+			handle_cmd(&env_copy, cmd);
 			free_tab(&cmd);
 			ft_printf("$> ");
 		}
